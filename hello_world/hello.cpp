184aeb831f310da604a44e9b405a2ef59eb2a18d
gohammdata <gohammdata@gmail.com>
2020-08-11 13:03:39 -0400

    Breaking down the anatomy of hello

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -1,7 +1,12 @@
+//  Preprocessor directive that includes header iostream
 #include <iostream>
 
+//  Start of the program function block main()
 int main()
 {
+    /* Write to the screen */
     std::cout   <<  "Hello World!";
+    
+    //  Returns a value to the OS
     return 0;
 }
